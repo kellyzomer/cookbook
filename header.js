1622b6573bbde4a1464747a27d6a9aa5f83d601b
Kelly Zomer <git@kellyzomer.com>
2023-04-01 12:18:05 +0200

    Created html, css and header for new repo

---
+++ header.js
@@ -0,0 +1,16 @@
+const template = document.createElement('template');
+template.innerHTML=`
+  <head>
+    <meta name="description" content="Koken met Kell">
+      <meta name="author" content="Kelly Zomer">
+      <meta name="robots" content="index, follow">
+      <link rel="stylesheet" href="/style.css">
+      <!-- Mobile friendly size -->
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+   </head>
+   <header>
+      <h1>Koken met Kell</h1>
+   </header>
+`
+
+document.body.prepend(template.content);
