b4b6cf27f677988a175e55b3b8817bb4cdc2cfaf
Kelly Zomer <git@kellyzomer.com>
2023-04-02 18:21:07 +0200

    Added recipes and a home button

--- header.js
+++ header.js
@@ -6,9 +6,10 @@ template.innerHTML=`
       <meta name="robots" content="index, follow">
       <link rel="stylesheet" href="/style.css">
       <!-- Mobile friendly size -->
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
    </head>
    <header>
+      <meta charset="utf-8">
       <h1>Koken met Kell</h1>
    </header>
 `
